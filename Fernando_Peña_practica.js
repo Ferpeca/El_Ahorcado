10659b519b38961efec805035664fd31ab3c77b6
Ferpeca <ferpeca95@gmail.com>
2023-05-11 16:45:28 +0200

    11/05/23

--- Fernando_Peña_practica.js
+++ Fernando_Peña_practica.js
@@ -18,27 +18,69 @@ let juegoTerminado = false; // indicador futuro para indicar si el juego ha term
 // Array de palabras para el juego
 const palabras = ['manzana', 'camiseta', 'mariposa', 'cadaver', 'vikingo', 'baloncesto', 'matrimonio'];
 
-// Selección de elementos del DOM
-const btn = id('jugar'); // Botón "jugar"
-const imagen = id('imagen'); // contenedor donde van a ir las fotos
-const btn_letras = document.querySelectorAll("#letras button"); // Botones de las letras
-const reiniciarBtn = id('reiniciar'); // Botón de reiniciar
-
-// Evento click para reiniciar el juego
-reiniciarBtn.addEventListener('click', reiniciar);
-//Evento click para iniciarlo
-btn.addEventListener('click', iniciar);
-
 
 //ACUERDATE!! es una función auxiliar que se utiliza para coger un elemento del DOM por su ID. El str representa el ID del elemento que se desea coger, y utiliza la función document.getElementById(str) para buscar y devolver ese elemento.
 function id(str) {
     return document.getElementById(str)
 }
 
-//CAMBIO DE ENFOQUE VOY HACERLO CON BOTONES PARA LAS LETRAS Y NO VOY HACER NIVELES PORQUE ME ESTOY ATASCANDO
-//CUANDO LEA ESTO VOY HACER EL METODO PARA QUE SALGA UNA PALABRA ALEATORIA Y SE SUSTITUYA POR GUIONES
-//LO TENGO ABAJO PERO NO FUNCIONABA.
-//HACER EL CONTADOR SIN EL ALERT, ES MUY MOLESTO PARA DESARROLLAR Y HACER PRUEBAS
+// Selección de elementos del DOM
+const btn = id('jugar'); // Botón jugar
+const imagen = id('imagen'); // Elemento de imagen
+const btn_letras = document.querySelectorAll("#letras button"); // Botones de letras
+const reiniciarBtn = id('reiniciar'); // Botón reiniciar
+
+// Este evento se activa cuando se hace clic en el botón de reinicio (reiniciarBtn). Al hacer clic, se llama a la función reiniciar
+reiniciarBtn.addEventListener('click', reiniciar);
+
+//Este evento se activa cuando se hace clic en el botón de jugar (btn). Al hacer clic, se llama a la función iniciar.
+btn.addEventListener('click', iniciar);
+
+//Utiliza el método querySelector para seleccionar el main.
+const main = document.querySelector('main');
+
+
+//Con este evento lo que hago es que cada vez que se presione la C se cambie el fondo del body a azul 
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'c') {
+        //Se utiliza el metodo getComputedStyle para coger el fondo que ya hay y se compara con el color a cambiar, si coincide se cambia a azul y si no a blanco
+        if (getComputedStyle(document.body).backgroundColor === 'rgb(173, 216, 230)') {
+            document.body.style.backgroundColor = ''; // Restaurar el color de fondo original del body
+        } else {
+            document.body.style.backgroundColor = 'rgb(173, 216, 230)'; // Cambiar el color de fondo del body a azul claro
+        }
+        //Se de tiene para que no haya propagacion a otros elementos
+        event.stopPropagation();
+
+        //Si la tecla no es "c" y es "D" se hace lo mismo que el anterior pero sin metodo ya que tenemos un color asignado 
+    } else if (event.key === 'd') {
+        if (main.style.backgroundColor === 'rgb(144, 238, 144)') {
+            main.style.backgroundColor = ''; // Restaurar el color de fondo original del main
+        } else {
+            main.style.backgroundColor = 'rgb(144, 238, 144)'; // Cambiar el color de fondo del main a verde claro
+        }
+        //Se de tiene para que no haya propagacion a otros elementos
+        event.stopPropagation();
+    }
+});
+
+btn.addEventListener('mouseover', function (event) {
+    event.target.style.backgroundColor = 'rgb(255, 182, 193)'; // Cambiar el color de fondo del botón "obtener palabra" a rosado claro
+});
+
+btn.addEventListener('mouseout', function (event) {
+    event.target.style.backgroundColor = ''; // Restaurar el color de fondo original del botón "obtener palabra"
+});
+
+reiniciarBtn.addEventListener('mouseover', function (event) {
+    event.target.style.backgroundColor = 'rgb(255, 255, 153)'; // Cambiar el color de fondo del botón "reiniciar" a amarillo claro
+});
+
+reiniciarBtn.addEventListener('mouseout', function (event) {
+    event.target.style.backgroundColor = ''; // Restaurar el color de fondo original del botón "reiniciar"
+});
+
+
 
 
 /*function iniciarJuego(palabrasSeleccionadas) {
